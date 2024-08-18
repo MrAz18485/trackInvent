47b341f81b2ec16abbef84231e21d7f82dea9d89
MrAz18485 <mraz18485@hotmail.com>
2024-08-18 16:51:53 +0300

    Updated

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 
-@app.route("/register")
+@app.route("/register", methods=["POST", "GET"])
 def register():
-    
+    if request.method == "GET":
+        render_template("register.html")
