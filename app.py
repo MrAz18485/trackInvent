28c336d2e25e2d8680d31d8bc8e7e9104d15cafe
MrAz18485 <mraz18485@hotmail.com>
2024-08-18 21:14:04 +0300

    Updated

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
 # using sqlite for sql db
 app = Flask(__name__)
 
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+
+
+@app.route("/login", methods=["POST", "GET"])
+def index():
+    return render_template("login.html")
 
 @app.route("/register", methods=["POST", "GET"])
 def register():
