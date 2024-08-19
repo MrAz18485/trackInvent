8f0ed7bcdd417853d163b3255d290fc16bacd441
MrAz18485 <mraz18485@hotmail.com>
2024-08-19 11:44:37 +0300

    Updates

--- app.py
+++ app.py
@@ -1,27 +1,46 @@
 # with the help of https://flask.palletsprojects.com/en/3.0.x/tutorial/factory/
+# https://www.geeksforgeeks.org/python-sqlite/ for helping me in sqlite
 
 import os
-from flask import Flask, render_template, request
+import hashlib
+from flask import Flask, render_template, request, redirect, session, flash
+from flask_session import Session
 from helpers import login_required
+import sqlite3
 # using sqlite for sql db
 app = Flask(__name__)
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
-
-
-@app.route("/login", methods=["POST", "GET"])
-def login():
-    if request.method == "GET":
-        return render_template("login.html")
-
-@app.route("/register", methods=["POST", "GET"])
-def register():
-    if request.method == "GET":
-        render_template("register.html")
 
 @app.route("/")
 @login_required
 def index():
     return render_template("index.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        session["email"] = request.form.get("emailField")
+        session["password"] = request.form.get("passwordField")
+        hashing = hashlib.sha256()
+        hashing.update(session["password"])
+        password_hash = hashing.hexdigest()
+        conn = sqlite3.connect("userinfo.db")
+        cursor = conn.cursor()
+        userInfo = cursor.execute("SELECT * FROM userinfo WHERE password = ?", password_hash)
+        print(userInfo)
+        if len(userInfo) == 0:
+            flash("Username or password does not exist!")
+            return render_template("login.html")
+        return render_template("index.html")
+
+
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
--- helpers.py
+++ helpers.py
@@ -1,10 +1,13 @@
+# with the help of https://flask.palletsprojects.com/en/latest/patterns/viewdecorators/
+
 from functools import wraps
-from flask import g, redirect, render_template
+from flask import g, redirect, render_template, request, session
 
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if g.user is None:
+        if session.get("user_id") is None:
             return redirect("/login")
         return f(*args, **kwargs)
+
     return decorated_function
